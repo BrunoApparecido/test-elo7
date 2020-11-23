fbefd291d07bccbaf1c364a44334c4e659e61c88
Bruno Alexandre <brunoapparecido@gmail.com>
2020-11-23 16:25:45 -0300

    adicionado header

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ const gulp = require('gulp');
 const minify = require('gulp-minify');
 const csso = require('gulp-csso');
 const concat = require('gulp-concat');
+const image = require('gulp-image');
 const browserSync = require('browser-sync').create();
 
 function cssMinify() {
-    return gulp.src('./src/css/*.css')
+    return gulp.src(['./src/css/*.css'])
         .pipe(csso())
         .pipe(concat('app.min.css'))
         .pipe(gulp.dest('./public/css')) 
@@ -30,6 +31,12 @@ function htmlCopy() {
     .pipe(gulp.dest('public'))
 }
 
+function optimizeImages() {
+    return gulp.src(['src/imgs/*.jpg','src/imgs/*.png'])
+    .pipe(image())
+    .pipe(gulp.dest('public/imgs'));
+}
+
 function serve() {
     browserSync.init({
         server: "./public"
@@ -37,9 +44,10 @@ function serve() {
 
     gulp.watch("src/css/*.css",cssMinify).on('change', browserSync.reload);;
     gulp.watch("src/js/*.js", jsMinify).on('change', browserSync.reload);;
+    gulp.watch("src/imgs/*", optimizeImages).on('change', browserSync.reload);;
     gulp.watch("src/*.html",htmlCopy).on('change', browserSync.reload);
 
 }
 
-exports.minify = gulp.parallel(jsMinify,cssMinify,htmlCopy); 
+exports.minify = gulp.parallel(jsMinify,cssMinify,htmlCopy,optimizeImages); 
 exports.serve = serve;
